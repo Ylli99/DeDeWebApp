9867f8a943364e2a405afd1ce31ef45b04f9d857
Ylli Neziri <ylli.neziri@covar.io>
2022-09-09 10:20:36 +0200

    remove other stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import './App.css'
 import App from './App';
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
 );
